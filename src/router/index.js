e31de82bd0bd71941a52d892815f7bdfb2670f9a
zhaoyongji <yongj_0304@163.com>
2019-02-15 19:56:39 +0800

    登陆的路由配置完毕

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login.vue'
 
 Vue.use(Router)
 
@@ -9,6 +10,10 @@ export default new Router({
       path: '/',
       name: 'home',
       
+    },{
+      name:'login',
+      path:'/login',
+      component:Login
     }
   ]
 })
